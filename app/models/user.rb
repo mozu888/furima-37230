49160f864cff7f19cc2b24f8380ec7ca9b24fb24
mozu888 <94517488+mozu888@users.noreply.github.com>
2021-12-06 19:34:07 +0900

    ユーザの新規登録の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カナを使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
   validates :dob, presence: true
+  with_options format: { with: /[a-z\d]{6,}/i, message: 'には英字と数字の両方を含めて設定してください' } do
+    validates :password
+    validates :password_confirmation
+  end
 end
