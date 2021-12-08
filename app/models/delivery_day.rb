8076b2ef78076a1e9321aaf2d40ef7cae9ee8e32
mozu888 <94517488+mozu888@users.noreply.github.com>
2021-12-08 14:25:32 +0900

    商品出品機能実装

--- app/models/delivery_day.rb
+++ app/models/delivery_day.rb
@@ -1,9 +1,7 @@
-class DeliveryDay < ApplicationRecord
-  self.date = [
-    { id: 1, name: '---' },
-    { id: 1, name: '3日で発送' },
-    { id: 1, name: '4~7日で発送' },
+class DeliveryDay < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },{ id: 2, name: '3日で発送' },{ id: 3, name: '4~7日で発送' },
   ]
-  include ActiveHash::Assotiations
+  include ActiveHash::Associations
   has_many :items
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,25 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  belongs_to :category
-  belongs_to :delivery_day
-  belongs_to :delivery_fee
-  belongs_to :prefecture
-  belongs_to :status
+  extend ActiveHash::Associations::ActiveRecordExtensions do
+    belongs_to :category
+    belongs_to :delivery_day
+    belongs_to :delivery_fee
+    belongs_to :prefecture
+    belongs_to :status
+  end
+
   has_one_attached :image
 
-  with_options presence: ture
+  with_options presence: true do
     validates :item_name
     validates :explanation
     validates :price
-
-  with_options numericality: { other_than: 1 , message: "can't be blank"}
+  end
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
     validates :category_id
     validates :delivery_day_id
     validates :delivery_fee_id
     validates :prefecture_id
     validates :status_id
+  end
+  belongs_to :user
 end
