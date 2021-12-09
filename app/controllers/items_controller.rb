2b6efe986422a1f112cf92313ae8b0f9f14a73dc
mozu888 <94517488+mozu888@users.noreply.github.com>
2021-12-09 16:59:02 +0900

    編集ページへの遷移の変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update, :move_to_index]
+  before_action :move_to_index, only: :edit
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -15,13 +17,10 @@ class ItemsController < ApplicationController
     end
   end
   def show
-    @item = Item.find(params[:id])
   end
   def edit
-    @item = Item.find(params[:id])
   end
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to item_path
@@ -32,5 +31,13 @@ class ItemsController < ApplicationController
   private 
   def item_params
     params.require(:item).permit(:item_name, :image, :explanation, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  def move_to_index
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
