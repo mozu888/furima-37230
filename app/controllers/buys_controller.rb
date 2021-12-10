124ae602aa56668a070b601975e29958b91cc5ca
mozu888 <94517488+mozu888@users.noreply.github.com>
2021-12-10 18:19:21 +0900

    購入ページの編集

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,4 +1,19 @@
 class BuysController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+    @buy = Buy.new
+  end
+  def create
+    @buy = Buy.new(buy_params)
+    if @buy.valid?
+      @buy.save
+      return redirect_to root_path
+    else
+      render :index
+    end
+  end
+  private
+  def buy_params
+    params.require(:order)
   end
 end
