291ba2ea20c70cfeecf465b6d1913401356bf971
mozu888 <94517488+mozu888@users.noreply.github.com>
2021-12-13 16:42:02 +0900

    rubocopの自動修正

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -5,34 +5,40 @@ class BuysController < ApplicationController
   def index
     @buy_delivery = BuyDelivery.new
   end
+
   def create
     @buy_delivery = BuyDelivery.new(buy_params)
     if @buy_delivery.valid?
       pay_item
       @buy_delivery.save
-      return redirect_to root_path
+      redirect_to root_path
     else
       render :index
     end
   end
+
   private
+
   def buy_params
-    params.require(:buy_delivery).permit(:token, :postal_code, :prefecture_id, :municipality, :address, :building, :call_number).merge(token: params[:token], user_id: current_user.id, item_id: @item.id)
+    params.require(:buy_delivery).permit(:token, :postal_code, :prefecture_id, :municipality, :address, :building, :call_number)
+      .merge(token: params[:token], user_id: current_user.id, item_id: @item.id
+    )
   end
+
   def set_buy
     @item = Item.find(params[:item_id])
   end
+
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @item.price,
       card: buy_params[:token],
       currency: 'jpy'
     )
   end
+
   def move_to_index
-    if current_user.id == @item.user_id || @item.buy.id
-      redirect_to root_path
-    end
+    redirect_to root_path if current_user.id == @item.user_id || @item.buy.id
   end
 end
--- app/models/buy_delivery.rb
+++ app/models/buy_delivery.rb
@@ -1,17 +1,19 @@
 class BuyDelivery
   include ActiveModel::Model
   attr_accessor :token, :postal_code, :prefecture_id, :municipality, :address, :building, :call_number, :user_id, :item_id
+
   with_options presence: true do
     validates :token
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :municipality
     validates :address
-    validates :call_number, format: {with: /\A[0-9]{10,11}\z/}
+    validates :call_number, format: { with: /\A[0-9]{10,11}\z/ }
     validates :user_id
   end
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   def save
     buy = Buy.create(user_id: user_id, item_id: item_id)
-    Delivery.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building: building, call_number: call_number, buy_id: buy.id)
+    Delivery.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address,
+                    building: building, call_number: call_number, buy_id: buy.id)
   end
 end
