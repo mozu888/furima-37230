72d4b2cb37d4979695c0c17ccb2febb0a3eda684
mozu888 <94517488+mozu888@users.noreply.github.com>
2021-12-14 14:56:00 +0900

    修正3

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -5,7 +5,7 @@ function input() {
   priceInput.addEventListener("input",function() {
     const inputValue = priceInput.value
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
-    salesProfit.innerHTML = Math.floor(inputValue * 1.1)
+    salesProfit.innerHTML = Math.floor(inputValue * 0.9)
   })
 }
 
